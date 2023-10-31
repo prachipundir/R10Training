403a06ef289d2dd7af2080ef4779b2ca27050f48
prachipundir <42332267+prachipundir@users.noreply.github.com>
2023-10-31 10:51:53 +0530

    Add files via upload
    Lambda Expression, Delegate

---
+++ DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DelegateExample
+{
+    delegate int MathOperation(int a, int b);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation addition=(x,y)=>x + y;
+            MathOperation subtract=(x,y)=>x - y;
+            MathOperation multiply=(x,y)=>x*y;
+
+            int fisrtNumber = 10;
+            int secondNumber = 5;
+
+            int sum= addition(fisrtNumber, secondNumber);
+            Console.WriteLine(sum);
+            int difference= subtract(fisrtNumber, secondNumber);
+            Console.WriteLine(difference);
+            int product=multiply(fisrtNumber, secondNumber);
+            Console.WriteLine(product);
+
+        }
+    }
+}
---
+++ LambdaExample/LambdaExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace LambdaExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            for(int i=0;i<10; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                list.Add(num);
+
+            }
+            Console.WriteLine("original list");
+            foreach(var number in list)
+            {
+                Console.WriteLine(number);
+            }
+
+            var evenNumbers = list.Where(x => x % 2 == 0);
+            Console.WriteLine("Even numbers in the list");
+            foreach (var number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
