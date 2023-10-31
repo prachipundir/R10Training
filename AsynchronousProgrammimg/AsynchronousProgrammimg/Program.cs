2f677cce9c66041a6566e489692c7c07b9dadd0a
prachipundir <42332267+prachipundir@users.noreply.github.com>
2023-10-31 12:35:10 +0530

    Add files via upload
    Asynchronous Programming
    Parallel Programming

---
+++ AsynchronousProgrammimg/AsynchronousProgrammimg/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AsynchronousProgrammimg
+{
+    internal class Program
+    {
+        async static void SomeMethod()
+        {
+            Console.WriteLine(" Some method Started!");
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("/n");
+            Console.WriteLine("Some method Exiting!");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Started!");
+            SomeMethod();
+            Console.WriteLine("Main method exiting!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ParallelProgrammimg/ParallelProgrammimg/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ParallelProgrammimg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Parallel for loop");
+            Parallel.For(1, 11, number => {
+                Console.WriteLine(number);
+            });
+
+            }
+        }
+    }
